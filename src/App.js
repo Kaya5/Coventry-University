38b2cae3fc1770fc3b55b522f3c643f4720c9490
Innocent Adedayo <deewai48@gmail.com>
2021-12-09 09:31:57 -0800

    use hash router

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import PageFour from "./pages/PageFour";
 import PageFive from "./pages/PageFive";
 
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
